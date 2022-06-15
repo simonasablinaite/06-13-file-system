4be045b4a37ee3aed62068cabc1e662648a027c7
simonasablinaite <simonasablinaite@gmail.com>
2022-06-15 10:39:13 +0300

    file-system: create file

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ import { file } from './lib/file.js';
 import { skaitomTest } from './lib/test.js';
 
 const testas = await skaitomTest()
-console.log(testas);
+//console.log(testas);
 
 // testas Nr.1: kai viskas nurodyta teisingai
 
 const readStatus = await file.read('users', 'petras.json');
-console.log('readStatus:', readStatus);
+//console.log('readStatus:', readStatus);
 
 // test Nr.2: nurodytas neteisingas failo pavadinimas
 
 // const readStatus2 = await file.read('users', 'maryte.json');
 // console.log('readStatus:', readStatus2);
 
-// const user = {
-//    name: 'Petras',
-//    age: 99,
-//    isLoggedIn: false,
-// }
+const userMaryte = {
+   name: 'Maryte',
+   age: 56,
+   isMarried: false,
+}
 
-// const status = file.create('users', 'petras.json', user);
-// console.log('File status:', status);
+const createStatus = await file.create('users', 'maryte.json', userMaryte);
+console.log('File status:', createStatus);
 
 
 
